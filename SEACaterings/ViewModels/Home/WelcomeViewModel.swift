b3e5c77294f5644ae1eac9c437d45aa4ff984405
Muhammad Ardiansyah Asrifah_H071221068 <113808797+ArdiansyahAsrifah@users.noreply.github.com>
2025-06-15 14:05:17 +0700

    feat - welcome view model

---
+++ SEACaterings/ViewModels/Home/WelcomeViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  WelcomeViewModel.swift
+//  SEACaterings
+//
+//  Created by Muhammad Ardiansyah Asrifah on 15/06/25.
+//
+
+import Foundation
+
+class WelcomeViewModel: ObservableObject {
+    let welcomeTitle = "Selamat Datang!"
+    let welcomeEmoji = "👋"
+    let welcomeMessage = """
+SEA Catering adalah layanan makanan sehat yang dapat disesuaikan dengan kebutuhan Anda. Kami menghadirkan makanan bergizi tinggi dengan pengiriman ke seluruh Indonesia, memastikan Anda tetap sehat dimanapun berada.
+"""
+}
