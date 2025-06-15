4f9fb8e6c36c53b96bbedcfca11a1ec6d15e1033
Muhammad Ardiansyah Asrifah_H071221068 <113808797+ArdiansyahAsrifah@users.noreply.github.com>
2025-06-15 14:24:10 +0700

    feat - contact view

---
+++ SEACaterings/View/Contact/ContactView.swift
@@ -0,0 +1,16 @@
+//
+//  ContactView.swift
+//  SEACaterings
+//
+//  Created by Muhammad Ardiansyah Asrifah on 15/06/25.
+//
+
+import SwiftUI
+
+struct ContactView: View {
+    var body: some View {
+        Text("Contact Us")
+            .font(.largeTitle)
+            .padding()
+    }
+}
