51d41213974130cfdf347330e5584e6724ce2420
Muhammad Ardiansyah Asrifah_H071221068 <113808797+ArdiansyahAsrifah@users.noreply.github.com>
2025-06-15 13:50:53 +0700

    Initial Commit

---
+++ SEACaterings/SEACateringsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SEACateringsApp.swift
+//  SEACaterings
+//
+//  Created by Muhammad Ardiansyah Asrifah on 15/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SEACateringsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SEACateringsTests/SEACateringsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SEACateringsTests.swift
+//  SEACateringsTests
+//
+//  Created by Muhammad Ardiansyah Asrifah on 15/06/25.
+//
+
+import Testing
+@testable import SEACaterings
+
+struct SEACateringsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
